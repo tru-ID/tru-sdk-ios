a520fcbe12daac5f3dd6cc920460e5682bb9df62
Murat Yakici <murat.yakici@dxwavetech.com>
2021-04-15 11:12:48 +0100

    Improving against force unwraps which may cause crashes.

--- Sources/TruSDK/CellularConnectionManager.swift
+++ Sources/TruSDK/CellularConnectionManager.swift
@@ -9,17 +9,23 @@ import os
 //
 // Force connectivity to cellular only
 // Open the "check url" and follows all redirects
-// Redirect Manager might switch from tls to non-tls between redirects
+// CellularConnectionManager might switch from tls to non-tls between redirects
 //
 @available(macOS 10.14, *)
 @available(iOS 13.0, *)
 class CellularConnectionManager {
 
+    let traceLog = OSLog(subsystem: "id.tru.sdk", category: "trace")
+
     let truSdkVersion = "0.0.13"
     private var connection: NWConnection?
 
+    //Mitigation for tcp timeout not triggering any events.
+    private var timer: Timer?
+    private let CONNECTION_TIME_OUT = 20.0
+
     // MARK: - Private utility methods
-    private func startConnection(url: URL) {
+    private func startConnection(scheme: String, host: String) {
         let pathMonitor = NWPathMonitor()
 
         pathMonitor.pathUpdateHandler = { path in
@@ -27,22 +33,22 @@ class CellularConnectionManager {
             for interfaceType in interfaceTypes {
                 switch interfaceType {
                 case .wifi:
-                    print("Path is Wi-Fi")
+                    os_log("Path is Wi-Fi")
                 case .cellular:
-                    print("Path is Cellular ipv4 \(path.supportsIPv4) ipv6 \(path.supportsIPv6)")
+                    os_log("Path is Cellular ipv4 %s ipv6 %s", path.supportsIPv4.description, path.supportsIPv6.description)
                 case .wiredEthernet:
-                    print("Path is Wired Ethernet")
+                    os_log("Path is Wired Ethernet")
                 case .loopback:
-                    print("Path is Loopback")
+                    os_log("Path is Loopback")
                 case .other:
-                    print("Path is other")
+                    os_log("Path is other")
                 default:
-                    print("Path is unknown")
+                    os_log("Path is unknown")
                 }
             }
         }
 
-        pathMonitor.start(queue: DispatchQueue(label: "tru.id.monitor"))
+        pathMonitor.start(queue: .main)
         
         let tcpOptions = NWProtocolTCP.Options()
         tcpOptions.connectionTimeout = 5 //Secs
@@ -50,36 +56,36 @@ class CellularConnectionManager {
 
         var tlsOptions: NWProtocolTLS.Options?
         var port = NWEndpoint.Port.http
-        if (url.scheme!.starts(with:"https")) {
+        if (scheme.starts(with:"https")) {
             port = NWEndpoint.Port.https
             tlsOptions = .init()
         }
-
-        TruIDLog(message: "connection scheme \(url.scheme!) \(port)")
+        os_log("", log: traceLog, type: .fault, "")
+        os_log("connection scheme %s %s", scheme, String(port.rawValue))
         // force network connection to cellular only
         let params = NWParameters(tls: tlsOptions , tcp: tcpOptions)
         params.requiredInterfaceType = .cellular
         params.prohibitExpensivePaths = false
         params.prohibitedInterfaceTypes = [.wifi]
         // create network connection
-        connection = NWConnection(host: NWEndpoint.Host(url.host!), port: port, using: params)
-        connection?.stateUpdateHandler = { (newState) in
+        connection = NWConnection(host: NWEndpoint.Host(host), port: port, using: params)
+        connection?.stateUpdateHandler = { [weak self] (newState) in
             switch (newState) {
             case .ready:
-                TruIDLog(message: "Connection State: Ready \(self.connection.debugDescription)\n")
-                os_log("Connection State: Ready %s", log: OSLog.default, type: .debug, self.connection.debugDescription)
+                os_log("Connection State: Ready %s\n", self?.connection.debugDescription ?? "No connection details")
             case .setup:
-                TruIDLog(message: "Connection State: Setup\n")
+                os_log("Connection State: Setup\n")
             case .cancelled:
-                TruIDLog(message: "Connection State: Cancelled\n")
+                os_log("Connection State: Cancelled\n")
             case .preparing:
-                TruIDLog(message: "Connection State: Preparing\n")
+                os_log("Connection State: Preparing\n")
+                self?.createTimer()
             case .failed(let error):
-                TruIDLog(message: "Connection State: Failed ->\(error)")
-                self.connection?.cancel()
+                os_log("Connection State: Failed ->%s", type:.error, error.localizedDescription)
+                self?.connection?.cancel()
             default:
-                TruIDLog(message: "Connection ERROR State not defined\n")
-                self.connection?.cancel()
+                os_log("Connection ERROR State not defined\n")
+                self?.connection?.cancel()
                 break
             }
         }
@@ -87,39 +93,71 @@ class CellularConnectionManager {
         connection?.start(queue: .main)
     }
 
+    func createTimer() {
+        if timer != nil {
+            timer?.invalidate()
+        }
+
+        timer = Timer.scheduledTimer(timeInterval: self.CONNECTION_TIME_OUT,
+                                    target: self,
+                                    selector: #selector(self.fireTimer),
+                                    userInfo: nil, repeats: false)
+    }
+
+    @objc func fireTimer() {
+        timer?.invalidate()
+        connection?.cancel()
+    }
+
     private func sendAndReceive(data: Data, completion: @escaping (Result<URL?, NetworkError>) -> Void) {
-        self.connection!.send(content: data, completion: NWConnection.SendCompletion.contentProcessed({ (error) in
+        connection?.send(content: data, completion: NWConnection.SendCompletion.contentProcessed({ (error) in
             if let err = error {
-                os_log("Sending error %s", log: OSLog.default, type: .debug, err.localizedDescription)
+                os_log("Sending error %s", type: .error, err.localizedDescription)
                 completion(.failure(.other(err.localizedDescription)))
 
             }
         }))
         // only reading the first 4Kb to retreive the Status & Location headers, not interested in the body
-        self.connection!.receive(minimumIncompleteLength: 1, maximumLength: 4096) { data, _, isComplete, error in
-            TruIDLog(message: "Receive isComplete: " + isComplete.description)
+        connection?.receive(minimumIncompleteLength: 1, maximumLength: 4096) { data, _, isComplete, error in
+
+            os_log("Receive isComplete: %s", isComplete.description)
+
             if let d = data, !d.isEmpty {
                 let response = String(data: d, encoding: .utf8)!
-                print(response)
-                guard let url = self.parseRedirect(response: response) else {
-                    completion(.failure(.invalidRedirectURL))
-                    return
+
+                os_log("Response:\n %s", response)
+
+                let status = self.httpStatusCode(response: response)
+                os_log("\n----\nHTTP status: %s", String(status))
+
+                switch status {
+                case 301...303, 307...308:
+                    guard let url = self.parseRedirect(response: response) else {
+                        completion(.failure(.invalidRedirectURL))
+                        return
+                    }
+                    completion(.success(url))
+                case 400...451:
+                    completion(.failure(.httpClient("HTTP Client Error:\(status)")))
+                case 500...511:
+                    completion(.failure(.httpServer("HTTP Server Error:\(status)")))
+                case 200...206:
+                    completion(.failure(.noRedirectURL))
+                default:
+                    completion(.failure(.other("HTTP Status can't be parsed \(status)")))
                 }
-                completion(.success(url))
+            } else {
+                completion(.failure(.noData))
             }
         }
     }
     
     private func parseRedirect(response: String) -> URL? {
-        let status = httpStatusCode(response: response)
-        TruIDLog(message: "\n----\nparseRedirect status: \(status)")
-        if 301...303 ~= status || 307...308 ~= status {
-            //header could be named "Location" or "location"
-            if let range = response.range(of: #"ocation: (.*)\r\n"#, options: .regularExpression) {
-                let location = response[range]
-                let redirect = location[location.index(location.startIndex, offsetBy: 9)..<location.index(location.endIndex, offsetBy: -1)]
-                return URL(string: String(redirect))
-            }
+        //header could be named "Location" or "location"
+        if let range = response.range(of: #"ocation: (.*)\r\n"#, options: .regularExpression) {
+            let location = response[range]
+            let redirect = location[location.index(location.startIndex, offsetBy: 9)..<location.index(location.endIndex, offsetBy: -1)]
+            return URL(string: String(redirect))
         }
         return nil
     }
@@ -145,22 +183,22 @@ class CellularConnectionManager {
     }
     
     private func sendAndReceiveDictionary(data: Data, completion: @escaping ([String : Any]?) -> Void) {
-        self.connection!.send(content: data, completion: NWConnection.SendCompletion.contentProcessed({ (error) in
+        connection?.send(content: data, completion: NWConnection.SendCompletion.contentProcessed({ (error) in
             if let err = error {
-                TruIDLog(message: "Sending error \(err)")
+                os_log("Sending error %", type:.error, err.localizedDescription)
                 completion(nil)
             }
         }))
 
-        self.connection!.receiveMessage { data, context, isComplete, error in
-            TruIDLog(message: "Receive isComplete: " + isComplete.description)
+        connection?.receiveMessage { data, context, isComplete, error in
+            os_log("Receive isComplete: %s", isComplete.description)
             guard let d = data else {
-                TruIDLog(message: "Error: Received nil data")
+                os_log("Error: Received nil data")
                 completion(nil)
                 return
             }
             let response = String(data: d, encoding: .utf8)!
-            print(response)
+            os_log("Response:\n%s", response)
             completion(self.parseJsonResponse(response: response))
         }
     }
@@ -177,20 +215,20 @@ class CellularConnectionManager {
                     // retrieve content
                     if let range = response.range(of: "\r\n\r\n") {
                         let content = response[range.upperBound..<response.index(response.endIndex, offsetBy: 0)]
-                        let json = String(content)
-                        let data = json.data(using: .utf8)
-                        var dict: [String : Any]? = nil
-                        // load JSON response into a dictionary
+                        let jsonString = String(content)
+                        var dict: [String : Any]?
+                        guard let data = jsonString.data(using: .utf8) else {
+                            return nil
+                        }
                         do {
-                            if let data = data {
-                                dict = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String : Any]
+                            // load JSON response into a dictionary
+                            dict = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String : Any]
+                            if let dict = dict {
+                                os_log("Dictionary: %s", dict.description)
                             }
                         } catch {
-                            //TODO: Let's catch
-                            TruIDLog(message: "JSON serialisation error: \(error)")
-                        }
-                        if let dict = dict {
-                            print("\(dict)")
+                            let msg = error.localizedDescription
+                            os_log("JSON serialisation error: %s", type:.error, msg)
                         }
                         return dict
                     }
@@ -212,26 +250,31 @@ class CellularConnectionManager {
 extension CellularConnectionManager: ConnectionManager {
 
     func openCheckUrl(url: URL, completion: @escaping (Any?) -> Void) {
-        TruIDLog(message: "opening \(url.absoluteString)")
-        startConnection(url: url)
-        let str = createHttpCommand(url: url)
-        TruIDLog(message: "sending data:\n\(str)")
-        let data: Data? = str.data(using: .utf8)
+        guard let scheme = url.scheme, let host = url.host else {
+            completion({})
+            return
+        }
+
+        os_log("opening %s", url.absoluteString)
+        startConnection(scheme: scheme, host: host)
+        let command = createHttpCommand(url: url)
+        os_log("sending data:\n%s", command)
+        let data = command.data(using: .utf8)
 
         sendAndReceive(data: data!) { (result) -> Void in
 
             switch result {
             case .success(let url):
                 if let redirectURL = url {
-                    TruIDLog(message: "redirect found: \(redirectURL)")
+                    os_log("redirect found: %s", redirectURL.absoluteString)
                     self.connection?.cancel()
                     self.openCheckUrl(url: redirectURL, completion: completion)
                 } else {
-                    TruIDLog(message: "openCheckUrl done")
+                    os_log("openCheckUrl done")
                     self.connection?.cancel()
                     completion({})
                 }
-            case .failure(let error):
+            case .failure(_):
                 completion({})
             }
 
@@ -240,9 +283,14 @@ extension CellularConnectionManager: ConnectionManager {
     }
 
     func jsonResponse(url: URL, completion: @escaping ([String : Any]?) -> Void)  {
-        startConnection(url: url)
+        guard let scheme = url.scheme, let host = url.host else {
+            completion(nil)
+            return
+        }
+
+        startConnection(scheme: scheme, host: host)
         let str = createHttpCommand(url: url)
-        TruIDLog(message: "sending data:\n\(str)")
+        os_log("sending data:\n", str)
 
         guard let data = str.data(using: .utf8) else {
             completion(nil)
@@ -255,9 +303,14 @@ extension CellularConnectionManager: ConnectionManager {
     }
 
     func jsonPropertyValue(for key: String, from url: URL, completion: @escaping (String) -> Void)  {
-        startConnection(url: url)
+        guard let scheme = url.scheme, let host = url.host else {
+            completion("")
+            return
+        }
+
+        startConnection(scheme: scheme, host: host)
         let str = createHttpCommand(url: url)
-        TruIDLog(message: "sending data:\n\(str)")
+        os_log("sending data:\n", str)
 
         guard let data = str.data(using: .utf8) else {
             completion("")
@@ -274,12 +327,6 @@ extension CellularConnectionManager: ConnectionManager {
     }
 }
 
-func TruIDLog(message: String) {
-    #if DEBUG
-    NSLog("[Tru.Id SDK] " + message)
-    #endif
-}
-
 protocol ConnectionManager {
     func openCheckUrl(url: URL, completion: @escaping (Any?) -> Void)
     func jsonResponse(url: URL, completion: @escaping ([String : Any]?) -> Void)
@@ -290,9 +337,12 @@ enum NetworkError: Error {
     case other(String)
     case invalidRedirectURL
     case tooManyRedirects
+    case noRedirectURL
     case connectionFailed(String)
-    case httpNotOK
+    case connectionCantBeCreated(String)
     case noData
+    case httpClient(String)
+    case httpServer(String)
 }
 
 
