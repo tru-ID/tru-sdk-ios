12c00d84468bfef3869a6d05534ab3cdab2d795b
Didem Yakici <didematilla@gmail.com>
2021-07-15 16:26:05 +0100

    Issue/4
    A method to serialise ReachabilityDetails to json string is added

--- Sources/TruSDK/ReachabilityDetails.swift
+++ Sources/TruSDK/ReachabilityDetails.swift
@@ -14,28 +14,45 @@ public struct ReachabilityDetails: Codable, Equatable {
     public let networkName: String
     public let products: [Product]?
     //let link: String
-
+    
     private enum CodingKeys : String, CodingKey {
         case countryCode = "country_code", networkId = "network_id", networkName = "network_name"
         case products
         //case links = "_links"
     }
-
+    
     public static func == (lhs: ReachabilityDetails, rhs: ReachabilityDetails) -> Bool {
         return lhs.countryCode == rhs.countryCode &&
             lhs.networkId == rhs.networkId &&
             lhs.networkName == lhs.networkName &&
             lhs.products == rhs.products
-            // && lhs.link == rhs.link
+        // && lhs.link == rhs.link
     }
-
+    
+    public func toJsonString() -> String {
+        var result = "{}"
+        
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .sortedKeys
+        
+        do {
+            let jsonData = try encoder.encode(self)
+            if let jsonString = String(data: jsonData, encoding: .utf8) {
+                result = jsonString
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        return result
+    }
 }
 
 /// The TruID products available for the application developer on the celluar network the app is connected to.
 public struct Product: Codable, Equatable {
     public let productId: String
     public let productType: ProductType
-
+    
     private enum CodingKeys : String, CodingKey {
         case productId = "product_id", productType = "product_name"
     }
@@ -55,4 +72,26 @@ public struct ReachabilityError: Error, Codable, Equatable {
     public let title: String
     public let status: Int
     public let detail: String
+    
+    private enum CodingKeys : String, CodingKey {
+        case type = "type", title = "title", status = "status", detail = "detail"
+        
+    }
+    
+    public func toJsonStringError() -> String {
+        var result = "{}"
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .sortedKeys
+        
+        do {
+            let jsonData = try encoder.encode(self)
+            if let jsonString = String(data: jsonData, encoding: .utf8) {
+                result = jsonString
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+        return result
+    }
 }
+
